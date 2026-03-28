a788a414a7cdfea0c7ba037a9feaa0160ae13da1
hj <rnfkfldu@gmail.com>
2026-03-29 00:57:30 +0900

    Auto commit: 2026-03-29 00:57:29

---
+++ src/screens/LabScreen.tsx
@@ -0,0 +1,7 @@
+export function LabScreen() {
+  return (
+    <div className="pt-4">
+      <p className="text-[var(--text)]">Lab - Coming soon</p>
+    </div>
+  )
+}
---
+++ src/screens/ScheduleScreen.tsx
@@ -0,0 +1,7 @@
+export function ScheduleScreen() {
+  return (
+    <div className="pt-4">
+      <p className="text-[var(--text)]">Schedule - Coming soon</p>
+    </div>
+  )
+}
